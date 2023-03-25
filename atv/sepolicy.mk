9230724b03629474f4ba2e2e847b9a78012f6f85
spkal01 <kalligeross@gmail.com>
2023-03-25 10:37:05 +0200

    Sepolicy: Adapt for spark
    Change-Id: I7ff74e2d96095a5d743b1a7d79310ec19dbff870

--- atv/sepolicy.mk
+++ atv/sepolicy.mk
@@ -1,12 +1,12 @@
 #
 # This policy configuration will be used by atv products that
-# inherit from Lineage
+# inherit from spark
 #
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/atv/vendor
+    device/spark/sepolicy/atv/vendor
 endif
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/private
+   device/spark/sepolicy/atv/private
